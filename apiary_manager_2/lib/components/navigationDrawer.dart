98fc0b55726ec60ab62cb1a325742e43d447922d
Maciej Fender <fendermaciej@gmail.com>
2022-04-06 23:35:20 +0200

    Dodanie main screen

--- apiary_manager_2/lib/components/navigationDrawer.dart
+++ apiary_manager_2/lib/components/navigationDrawer.dart
@@ -25,59 +25,89 @@ class NavigationDrawer extends StatelessWidget {
   }
 
   Widget buildHeader(BuildContext context) => DrawerHeader(
-      margin: EdgeInsets.zero,
-      padding: EdgeInsets.zero,
-      // decoration: BoxDecoration(
-      //   color: Colors.orange,
-      // ),
-      child: Stack(children: <Widget>[
-        Positioned(
-            bottom: 12.0,
-            left: 16.0,
-            child: Text("Apiary Manager",
+        margin: EdgeInsets.zero,
+        padding: EdgeInsets.zero,
+        // decoration: BoxDecoration(
+        //   color: Colors.orange,
+        // ),
+        child: Stack(
+          children: const <Widget>[
+            Positioned(
+              bottom: 12.0,
+              left: 16.0,
+              child: Text(
+                "Apiary Manager",
                 style: TextStyle(
-                    // color: Colors.white,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w500))),
-      ]));
+                  // color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
 
   Widget buildMenuItems(BuildContext context) => Column(
         children: [
           ListTile(
-            leading: Icon(Icons.home_outlined),
-            title: Text("Home"),
+            leading: const Icon(Icons.home_outlined),
+            title: const Text("Home"),
+            onTap: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.all_inbox_outlined),
+            title: const Text("Apiaries"),
             onTap: () {
               Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          HomeScreen()));
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ApiariesScreen(),
+                ),
+              );
             },
           ),
           ListTile(
-            leading: Icon(Icons.task_alt),
-            title: Text("Apiaries"),
+            leading: const Icon(Icons.task_alt),
+            title: const Text("Tasks"),
             onTap: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => ApiariesScreen()));
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const TasksScreen(),
+                ),
+              );
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: const Icon(Icons.settings_outlined),
+            title: const Text("Settings"),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SettingsScreen(),
+                ),
+              );
             },
           ),
           ListTile(
-            leading: Icon(Icons.task_alt),
-            title: Text("Tasks"),
+            leading: const Icon(Icons.account_circle_outlined),
+            title: const Text("Credits"),
             onTap: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => TasksScreen()));
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SettingsScreen(),
+                ),
+              );
             },
           ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text("Settings"),
-            onTap: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => SettingsScreen()));
-            },
-          ),
+
         ],
       );
 //pushReplacement
--- apiary_manager_2/lib/mainScreen.dart
+++ apiary_manager_2/lib/mainScreen.dart
@@ -7,7 +7,6 @@ import 'components/navigationDrawer.dart';
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
@@ -20,13 +19,34 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('Apiary Manager 2'),
       ),
       body: Center(
-        child: Column(
-          children: [
-            Text(AppLocalizations.of(context)!.helloWorld),
-          ],
-        ),
+        child: renderBody(context),
       ),
       drawer: NavigationDrawer(),
+    );
+  }
+
+  Widget renderBody(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, position) {
+        return ApiaryFragment(context, position);
+      },
+      itemCount: 10,
+    );
+  }
+
+  Widget ApiaryFragment(BuildContext context, int position) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 2, 0, 2),
+      child: Card(
+        child: InkWell(
+          splashColor: Colors.orange[30],
+          child: ListTile(
+            leading: Icon(Icons.all_inbox),
+            title: Text('The Enchanted Nightingale'),
+            subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+          ),
+        ),
+      ),
     );
   }
 }
